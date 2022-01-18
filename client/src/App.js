66a85d3c99b9ff413a0fe5ef2250b25eba1fc1b5
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2022-01-18 00:40:21 -0500

    Create TopNav component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import NavBar from './components/nav/NavBar';
+import Overview from './page/Overview';
 
 function App() {
   return (
     <div className="App min-h-screen pb-[72px] overflow-y-hidden">
-        Lol
+        <Overview />
         <NavBar />
     </div>
   );
---
+++ client/src/components/nav/TopNav.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+// TODO: Refractor where I can use this code as a base and wrap elements in it
+const TopNav = ({ title, icon, iconClickFunction }) => {
+    return (
+        <div class="h-24 mx-3 flex justify-between items-center">
+            <div class="flex items-center">
+                <h1 class="text-4xl font-bold text-[#4B4B4B]">{title}</h1>
+            </div>
+            {/* TODO: Make this more pratical  */}
+            <button onClick={()=> iconClickFunction()}>
+                {icon}
+            </button>
+        </div>
+    )
+}
+
+export default TopNav
