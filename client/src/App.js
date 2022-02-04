9baf300c8269ba638081a48fd6faad58d6e8846f
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2022-02-03 20:47:40 -0500

    Updated dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -4,9 +4,12 @@ import Chat from './page/Chat';
 import ChatScreen from './page/chat/ChatScreen';
 import Overview from './page/Overview';
 
+import 'react-chat-elements/dist/main.css';
+
 function App() {
   return (
-    <div className="App min-h-screen pb-[72px] overflow-y-hidden">
+    // <div className="App min-h-screen pb-[72px] overflow-y-hidden">
+    <div className="App min-h-screen overflow-y-hidden">
         {/* <Overview /> */}
         {/* <Chat /> */}
         <ChatScreen />
